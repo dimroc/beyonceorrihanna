b711c23e148cc6db189fe3c0c8f1d278cfc18040
Dimitri Roche <dimroc@gmail.com>
2011-12-02 21:17:38 -0500

    Validation of ip format in vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,9 +1,15 @@
 class Vote < ActiveRecord::Base
+  IP_REGEX = Regexp.union(["0.0.0.0", /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/])
+
   belongs_to :rivalry_character, counter_cache: :votes_count
   has_one :rivalry, through: :rivalry_character
 
   validates :rivalry_character_id, presence: true
-  validates :ip, presence: true, uniqueness: { scope: :rivalry_character_id }
+  validates :ip,
+            presence: true,
+            uniqueness: {scope: :rivalry_character_id},
+            format: {with: IP_REGEX}
+
   validate :ensure_unique_ip_per_rivalry
 
   private
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -14,6 +14,24 @@ describe Vote do
       it { should validate_uniqueness_of :ip, scope: :rivalry_character_id }
     end
 
+    context "ensures the format of the ip" do
+      subject { Factory.build(:vote, ip: ip) }
+      context "with a valid ip" do
+        let(:ip) { Faker::Internet.ip_v4_address }
+        it { should be_valid }
+      end
+
+      context "with an invalid ip of letters" do
+        let(:ip) { "www.123.www.w" }
+        it { should be_invalid }
+      end
+
+      context "with an invalid ip of numbers" do
+        let(:ip) { "111111111" }
+        it { should be_invalid }
+      end
+    end
+
     context "ensure uniqueness of ip per rivalry" do
       subject { Factory.build(:vote, rivalry_character: rivalry_character, ip: ip) }
       let(:ip) { Faker::Internet.ip_v4_address }
